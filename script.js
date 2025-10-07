a85f696581d5017c53c194102ce7b35742c8ca15
superwhooo <105936949+superwhooo@users.noreply.github.com>
2025-10-07 08:39:45 +0530

    Delete script.js
    Signed-off-by: superwhooo <105936949+superwhooo@users.noreply.github.com>

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector(".sayhey-btn");
-  btn.addEventListener("click", () => {
-    window.location.href = "onboarding2.html"; // proceed to next screen
-  });
-});
